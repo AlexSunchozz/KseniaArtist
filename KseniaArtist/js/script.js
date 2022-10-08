d58a230a42d916143b5851d085f25fd341b911aa
VityaPain <vitayzablockyy58@gmail.com>
2022-10-08 23:23:22 +0300

    about_ready_90%

--- KseniaArtist/js/script.js
+++ KseniaArtist/js/script.js
@@ -4,3 +4,13 @@ burger.addEventListener('click', ()=>{
     burger.classList.toggle('active');
     menu.classList.toggle('active');
 })
+
+// slider on index
+// СЛАЙДЕР
+$('.religious-slider').slick({
+    centerMode: true,
+    // centerPadding: '80px',
+    slidesToShow: 3,
+    // adaptiveHeight: true,
+    // variableWidth: true,
+});
