155419939f00b823882544949bb0bb019da3aec5
AlexandrRepkin <arepkin96@mail.ru>
2022-10-09 00:39:27 +0300

    showcase__end

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,8 +1,10 @@
 const burger = document.querySelector('.header-burger'),
-      menu = document.querySelector('.menu');
+      menu = document.querySelector('.menu'),
+      body = document.body;
 burger.addEventListener('click', ()=>{
     burger.classList.toggle('active');
     menu.classList.toggle('active');
+    body.classList.toggle('lock');
 })
 
 // slider on index
